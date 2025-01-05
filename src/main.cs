281a4e3c249281887756af947e0cf358dbc56759
dplocki <dawid.plocki@gmail.com>
2025-01-05 08:34:43 +0100

    feat: print the prompt

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,4 @@
-using System.Net;
-using System.Net.Sockets;
-
-// Uncomment this line to pass the first stage
-// Console.Write("$ ");
+Console.Write("$ ");
 
 // Wait for user input
 Console.ReadLine();

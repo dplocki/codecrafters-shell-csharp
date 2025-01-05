7bbc981654a354bb586d8a3ef1545c66e55c8df2
dplocki <dawid.plocki@gmail.com>
2025-01-05 08:38:17 +0100

    feat: implement the rapl

--- src/main.cs
+++ src/main.cs
@@ -1,6 +1,9 @@
 Console.Write("$ ");
 
-// Wait for user input
-var command = Console.ReadLine();
+while(true)
+{
+    // Wait for user input
+    var command = Console.ReadLine();
 
-Console.WriteLine($"{command}: command not found");
+    Console.WriteLine($"{command}: command not found");
+}

c92044e5b0af8577c0946b864bfe9c72ae3ac1b8
dplocki <dawid.plocki@gmail.com>
2025-01-11 20:09:19 +0100

    style: move the project's code into separate directory

--- your_program.sh
+++ your_program.sh
@@ -14,7 +14,7 @@ set -e # Exit early if any commands fail
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  dotnet build --configuration Release --output /tmp/codecrafters-build-csharp codecrafters-shell.csproj
+  dotnet build --configuration Release --output /tmp/codecrafters-build-csharp codecrafters-shell/codecrafters-shell.csproj
 )
 
 # Copied from .codecrafters/run.sh

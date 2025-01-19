37aa78ea769a15acfc1a4d19249fe7361c61392f
dplocki <dawid.plocki@gmail.com>
2025-01-19 14:04:49 +0100

    refactor(user input parser): implement partial completion

--- codecrafters-shell/SimpleUserInput.cs
+++ codecrafters-shell/SimpleUserInput.cs
@@ -24,17 +24,17 @@ class SimpleUserInput(IEnumerable<string> buildInCommands, ExecutableDirectories
             }
             else if (keyInfo.Key == ConsoleKey.Tab)
             {
+                var currentInput = input.ToString();
                 if (suggestions != null)
                 {
                     Console.WriteLine();
                     Console.WriteLine(string.Join("  ", suggestions));
                     Console.Write(Prompt);
-                    Console.Write(input.ToString());
+                    Console.Write(currentInput);
                     suggestions = null;
                     continue;
                 }
 
-                var currentInput = input.ToString();
                 var suggestion = buildInCommands.FirstOrDefault(s => s != null && s.StartsWith(currentInput));
                 if (suggestion == null)
                 {
@@ -42,6 +42,13 @@ class SimpleUserInput(IEnumerable<string> buildInCommands, ExecutableDirectories
                     if (suggestions.Count() == 1)
                     {
                         suggestion = suggestions.First();
+                    }
+                    else
+                    {
+                        suggestion = GetCommonPartInSuggestions(currentInput, suggestions);
+                        input.Append(suggestion);
+                        Console.Write(suggestion);
+                        continue;
                     }
                 }
                 if (suggestion == null)
@@ -64,5 +71,37 @@ class SimpleUserInput(IEnumerable<string> buildInCommands, ExecutableDirectories
         }
 
         return input.ToString();
+    }
+
+    private string? GetCommonPartInSuggestions(string currentInput, IEnumerable<string> suggestions)
+    {
+        var result = new StringBuilder();
+        var index = currentInput.Length;
+
+        while (true)
+        {
+            var letters = suggestions
+                .Select(suggestion => {
+                    if (index >= suggestion.Length)
+                    {
+                        return (char?)null;
+                    }
+
+                    return suggestion[index];
+                })
+                .Distinct()
+                .ToArray();
+
+            if (letters.Length == 1 && letters.First() != null)
+            {
+                result.Append(letters.First());
+            }
+            else
+            {
+                return result.ToString();
+            }
+
+            index++;
+        }
     }
 }
